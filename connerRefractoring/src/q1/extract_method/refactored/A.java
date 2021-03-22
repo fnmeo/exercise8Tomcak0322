f2fc666e5a1e4da1ced5d9cef9f1e932fdff5033
fnmeo <cltomcak@gmail.com>
2021-03-22 15:43:01 -0500

    commit

---
+++ connerRefractoring/src/q1/extract_method/refactored/A.java
@@ -0,0 +1,40 @@
+package q1.extract_method.refactored;
+
+import java.util.List;
+
+public class A {
+   Node m1(List<Node> nodes, String p) {
+	  printContains(nodes, p);
+      return null;
+   }
+
+   Edge m2(List<Edge> edgeList, String p) {
+      printContains(edgeList, p);
+      return null;
+   }
+
+   <T extends Graph> void printContains(List<T> objs, String p) {
+	      for (T obj : objs) {
+	         if (obj.contains(p))
+	            System.out.println(obj);
+	      }
+	   }
+}
+
+class Graph 
+{
+	   String name;
+
+	   public boolean contains(String p) 
+	   {
+	      return name.contains(p);
+	   }
+
+}
+
+
+class Node extends Graph {
+}
+
+class Edge extends Graph {
+}
